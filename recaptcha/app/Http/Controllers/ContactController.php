ec567e4a636f3452103bc89b9e49fe332e3611fe
shang9x123 <shang9x@gmail.com>
2022-08-18 15:22:43 +0700

    demo recaptcha

---
+++ recaptcha/app/Http/Controllers/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use App\Http\Requests\StoreContactRequest;
+use App\Http\Requests\UpdateContactRequest;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('contact.index');
+    }
+     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreContactRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreContactRequest $request)
+    {
+        //dd($request);
+        return view('contact.store');
+    }
+}
---
+++ recaptcha/resources/views/contact/store.blade.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<script>
+    function onSubmit(token) {
+        document.getElementById("form").submit();
+    }
+</script>
+<h3 style="text-align: center;text-transform: uppercase;">Demo Google reCaptcha của <a target="_blank" href="https://lashare.info"> LASHARE.info</a></h3>
+<h2 style="text-align: center;text-transform: uppercase;">Tuyệt vời bạn đã test thành công Google reCaptcha</a></h2>
+
+
